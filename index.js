490f48bf8c3a17edc5776302465df27e25ce0d07
gonzalo10 <gonzalo.achiaga@gmail.com>
2020-04-05 13:44:15 +0200

    feat: updated proxy server

--- index.js
+++ index.js
@@ -4,25 +4,7 @@ const app = express();
 
 const port = 5000;
 
-// Body parser
-app.use(express.urlencoded({ extended: false }));
-
-// Home route
 app.use('/', proxy());
-
-// Mock API
-app.get('/users', (req, res) => {
-	res.json([
-		{ name: 'William', location: 'Abu Dhabi' },
-		{ name: 'Chris', location: 'Vegas' }
-	]);
-});
-
-app.post('/user', (req, res) => {
-	const { name, location } = req.body;
-
-	res.send({ status: 'User created', name, location });
-});
 
 // Listen on port 5000
 app.listen(port, () => {
